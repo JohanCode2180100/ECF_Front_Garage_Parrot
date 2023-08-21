15540fa6ce02019daf6fd322f2f23add88e9e968
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-08-21 10:53:51 +0200

    add components folder, header component, burger-menu component and ng add angular/materials. Add a font family Rubik, lato and sans-serif

---
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-burger-menu",
+  template: ` <p>burger-menu works!</p> `,
+  styleUrls: ["./burger-menu.component.css"],
+})
+export class BurgerMenuComponent {}
