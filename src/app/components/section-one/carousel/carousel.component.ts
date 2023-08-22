6ab1e677853ccb27459556ee41dedabac147e6d2
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-08-22 23:38:49 +0200

    add carousel, contain with responsive screen

---
+++ src/app/components/section-one/carousel/carousel.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-carousel",
+  template: `
+    <div class="picture">
+      <img [src]="pictures" alt="images de mécanique voiture" />
+    </div>
+  `,
+  styleUrls: ["./carousel.component.css"],
+})
+export class SectionOneCarouselComponent {
+  pictures: string[] = ["assets/picture/carEngine.jpg"];
+}
