98eb040f9fb3837a135f95794deb7e409717f096
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-08-23 21:59:37 +0200

    add copyright element and add responsive screen

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,23 +4,26 @@ import { Component } from "@angular/core";
   selector: "app-footer",
   template: `
     <footer>
-      <div class="sectionFooter1">
-        <div class="picture">
-          <img
-            src="../../../assets/picture/logoGarageParrot.png"
-            alt="logo du garage de Monsieur Parrot"
-          />
+      <div class="container">
+        <div class="sectionFooter1">
+          <div class="picture">
+            <img
+              src="../../../assets/picture/logoGarageParrot.png"
+              alt="logo du garage de Monsieur Parrot"
+            />
+          </div>
+          <div class="adress">
+            <p>0322303030</p>
+            <br />
+            <p>1 RUE TOULOUSE</p>
+            <p>31000 TOULOUSE</p>
+          </div>
         </div>
-        <div class="adress">
-          <p>0322303030</p>
-          <br />
-          <p>1 RUE TOULOUSE</p>
-          <p>31000 TOULOUSE</p>
+        <div class="footer2">
+          <app-hours></app-hours>
         </div>
       </div>
-      <div class="footer2">
-        <app-hours></app-hours>
-      </div>
+      <p class="copy">Copyright ©Garage V.PARROT 2023</p>
     </footer>
   `,
   styleUrls: ["./footer.component.css"],
