a81998b967bd4123c7ba415383d37348d846c8d2
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-08-28 12:18:00 +0200

    add form validator and bordercolor directive

--- src/app/components/contact-page-information/contact-page-information.component.ts
+++ src/app/components/contact-page-information/contact-page-information.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -14,9 +14,65 @@ import { NgForm } from "@angular/forms";
 
       <div class="formContainer">
         <form #form="ngForm" (submit)="onSubmit(form)" class="form">
-          <input ngModel type="text" name="Nom" id="name" placeholder="Nom*" />
+          <div class="error">
+            <p
+              class="invalid-feedback"
+              *ngIf="name.touched && name.hasError('required')"
+            >
+              Le nom est obligatoire
+            </p>
+            <span
+              class="invalid-feedback"
+              *ngIf="firstName.touched && firstName.hasError('required')"
+            >
+              Le prénom est obligatoire
+            </span>
+            <span
+              class="invalid-feedback"
+              *ngIf="firstName.touched && firstName.hasError('required')"
+            >
+              L'adresse' est obligatoire
+            </span>
+            <span
+              class="invalid-feedback"
+              *ngIf="email.touched && email.hasError('required')"
+            >
+              L'adresse mail est obligatoire
+            </span>
+            <span
+              class="invalid-feedback"
+              *ngIf="email.touched && email.hasError('email')"
+            >
+              L'adresse mail est invalide
+            </span>
+            <p class="invalid-feedback" *ngIf="phone.touched && phone.invalid">
+              Le numéro de téléphone est obligatoire et doit contenir 10
+              chiffres
+            </p>
+            <p
+              class="invalid-feedback"
+              *ngIf="message.touched && message.invalid"
+            >
+              Vous devez inclure un message pour valider ce formulaire
+            </p>
+          </div>
+
           <input
+            required
+            name
             ngModel
+            #name="ngModel"
+            type="text"
+            name="Nom"
+            id="name"
+            placeholder="Nom*"
+          />
+
+          <input
+            required
+            firstName
+            ngModel
+            #firstName="ngModel"
             type="text"
             name="Prénom"
             id="firstName"
@@ -24,16 +80,21 @@ import { NgForm } from "@angular/forms";
           />
 
           <input
+            required
+            adress
             ngModel
+            #adress="ngModel"
             type="text"
             name="Adresse"
             id="address"
             placeholder="Adresse*"
           />
+
           <input
             required
             email
             ngModel
+            #email="ngModel"
             type="Email"
             name="Email"
             id="email"
@@ -41,15 +102,22 @@ import { NgForm } from "@angular/forms";
           />
 
           <input
+            required
+            phone
             ngModel
-            type="tel"
+            #phone="ngModel"
+            type="phone"
+            pattern="[0-9]{10}"
             name="Téléphone"
             id="phone"
             placeholder="téléphone*"
           />
 
           <textarea
+            required
+            message
             ngModel
+            #message="ngModel"
             rows="8"
             cols="28"
             type="textArea"
@@ -58,7 +126,13 @@ import { NgForm } from "@angular/forms";
             placeholder="Votre message*"
           ></textarea>
           <div class="button">
-            <button class="btn">Envoyer le formulaire</button>
+            <button
+              [ngClass]="{ 'disabled-btn': form.invalid }"
+              class="btn"
+              [disabled]="form.invalid"
+            >
+              Envoyer le formulaire
+            </button>
           </div>
         </form>
       </div>
@@ -67,6 +141,9 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./contact-page-information.css"],
 })
 export class ContactPageInformationComponent {
+  @ViewChild("email")
+  emailInput?: ElementRef<HTMLInputElement>;
+
   onSubmit(form: NgForm) {
     console.log(form);
   }
