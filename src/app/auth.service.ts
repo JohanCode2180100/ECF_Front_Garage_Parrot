e3f44a186c9ca990b0ee3398d7efe0b15b324f97
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-09-13 17:57:14 +0200

    add baseUrl for postLogin

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,25 +1,49 @@
 import { Injectable } from "@angular/core";
-import { Observable, delay, of, tap } from "rxjs";
+import { Observable, of } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  
-
   isLoggedIn: boolean = false;
   redirectUrl: string;
 
-  login(Email: string, Password: string): Observable<boolean> {
-    const isLoggedIn = Email == "aa" && Password == "aa";
+  constructor(private http: HttpClient) {}
 
-    return of(isLoggedIn).pipe(
-      delay(1000),
-      tap((isLoggedIn) => (this.isLoggedIn = isLoggedIn))
+  login(Email: string, Password: string): Observable<boolean> {
+    // Créez un objet contenant les données d'authentification
+    const authData = {
+      username: Email,
+      password: Password,
+    };
+
+    const baseURL = "http://localhost:3000/api/";
+
+    return this.http.post<any>(`${baseURL}login`, authData).pipe(
+      map((response) => {
+        // Si la réponse du serveur contient un JWT
+        if (response && response.token) {
+          // Stockez le JWT dans le stockage local ou dans un service d'authentification
+          localStorage.setItem("jwtToken", response.token);
+          this.isLoggedIn = true;
+          return true;
+        } else {
+          this.isLoggedIn = false;
+          return false;
+        }
+      }),
+      catchError((error) => {
+        this.isLoggedIn = false;
+        return of(false);
+      })
     );
   }
 
   logout() {
+    // Supprimez le JWT du stockage local ou du service d'authentification
+    localStorage.removeItem("jwtToken");
     this.isLoggedIn = false;
   }
 }
