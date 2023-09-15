2624920ec6e19a687a8a457dc0d6bfea79662e9a
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-09-15 03:22:48 +0200

    fix bug login const authData = {       Email: Email,       Password: Password,     };

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Observable, of } from "rxjs";
-import { HttpClient } from "@angular/common/http";
-import { catchError, map, tap } from "rxjs/operators";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { catchError, map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -15,17 +15,23 @@ export class AuthService {
   login(Email: string, Password: string): Observable<boolean> {
     // Créez un objet contenant les données d'authentification
     const authData = {
-      username: Email,
-      password: Password,
+      Email: Email,
+      Password: Password,
     };
 
-    const baseURL = "http://localhost:3000/api/";
+    const baseURL = "http://localhost:3000/api/login";
 
-    return this.http.post<any>(`${baseURL}login`, authData).pipe(
+    // Créez les en-têtes avec le jeton d'authentification
+    const token = localStorage.getItem("jwtToken");
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
+    // Utilisez les en-têtes lors de la réalisation de la requête HTTP
+    return this.http.post<any>(`${baseURL}`, authData, { headers }).pipe(
       map((response) => {
-        // Si la réponse du serveur contient un JWT
         if (response && response.token) {
-          // Stockez le JWT dans le stockage local ou dans un service d'authentification
+          // Stockez le JWT dans le stockage local
           localStorage.setItem("jwtToken", response.token);
           this.isLoggedIn = true;
           return true;
