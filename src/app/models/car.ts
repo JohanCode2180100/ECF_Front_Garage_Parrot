036ffdb25454ccbc37a7a89ad208c8c33a07282b
JohanCode2180100 <Johan_CODE21@orange.fr>
2023-09-20 10:37:12 +0200

    add car interface

---
+++ src/app/models/car.ts
@@ -0,0 +1,11 @@
+export interface car {
+  Second_hand_car_id: number;
+  Brand: string;
+  Name: string;
+  Year: number;
+  Kilometer: number;
+  Price: string;
+  Picture: string;
+  Description: string;
+  CreatedAt: Date;
+}
