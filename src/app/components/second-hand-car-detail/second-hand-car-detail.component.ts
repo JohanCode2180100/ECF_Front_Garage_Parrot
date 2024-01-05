ee668561cef65b63c13dc313afdb8119ac6df1ce
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-05 15:34:54 +0100

    changed color price and padding and margin button carId

--- src/app/components/second-hand-car-detail/second-hand-car-detail.component.ts
+++ src/app/components/second-hand-car-detail/second-hand-car-detail.component.ts
@@ -11,7 +11,7 @@ import { carsService } from "src/app/services/cars.service";
       <h2>{{ carByID.brand }} {{ carByID.name }} - {{ carByID.year }}</h2>
       <img [src]="carByID.picture" alt="image d'une voiture d'occasions" />
       <p>{{ carByID.kilometer }} km</p>
-      <p>{{ carByID.price | currency : "EUR" : "symbol" }}</p>
+      <p class="price">{{ carByID.price | currency : "EUR" : "symbol" }}</p>
       <p>{{ carByID.description }}</p>
 
       <mat-card-actions class="cardAction">
