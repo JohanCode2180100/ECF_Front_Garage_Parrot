07ab142d7a3936476f408e68c6942045812e2a52
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-21 08:57:40 +0100

    add prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   apiUrl: "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/",
   api: "api/",
+  apiUrlAdminApi:
+    "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/admin/api/",
 };
