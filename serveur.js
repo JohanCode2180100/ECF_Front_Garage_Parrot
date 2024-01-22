2202182bd6ceb6c5b8c427db7201e6a5fcf7ee9b
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-22 22:20:56 +0100

    add engines files

--- serveur.js
+++ serveur.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 let app = express();
 
 app.use(express.static(__dirname + "/dist/garage-parrot"));
 
 app.get("/*", (req, res) => {
-  res.sendFile(__dirname + "dist/garage-parrot/index.html");
+  res.sendFile(path.join(__dirname + "dist/garage-parrot/index.html"));
 });
 
 app.listen(process.env.PORT || 8080);
