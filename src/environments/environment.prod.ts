85bbd49fd222c2b988a7898df00a20bdba809679
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-23 17:56:12 +0100

    updated and replaces files angular.json

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const env = {
-  production: true,
-  apiURL: "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/api/",
-  apiURLadmin:
-    "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/admin/api/",
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const env = {
-  production: false,
-  apiURL: "http://localhost:3000/api/",
-  apiURLadmin:"http://localhost:3000/admin/api/"
+  production: true,
+  apiURL: "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/api/",
+  apiURLadmin:
+    "https://garageparrotbackend-29c911d2d7f6.herokuapp.com/admin/api/",
+  
 };
