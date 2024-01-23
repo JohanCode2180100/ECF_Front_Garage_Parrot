5edc02f4cd73a0c6b84ad99a8f5ea2f7ca897286
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-23 20:50:09 +0100

    add formContactService

---
+++ src/app/services/form-contact.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { env } from "src/environments/environment";
+import { FormContact } from "../models/formContact";
+
+@Injectable({
+  providedIn: "root",
+})
+export class FormContactService {
+  constructor(private http: HttpClient) {}
+  private formUrl = env.apiURL;
+
+  // ADD FORM----------------------------------------------------------------------------
+  addPostForm(FormContact: FormContact): Observable<FormContact> {
+    return this.http.post<FormContact>(`${this.formUrl}/contact`, FormContact);
+  }
+}
