debbd1c08a4c53b83cf279026cc5df791fe9dc27
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-01-30 22:29:18 +0100

    add form field style for component and add button for back as menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SecondHandCarComponent } from "./components/second-hand-car/second-hand-car/second-hand-car.component";
-
 import { HomeComponent } from "./components/home/home.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { ContactPageInformationComponent } from "./components/contact-page-information/contact-page-information.component";
