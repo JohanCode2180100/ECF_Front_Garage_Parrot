4bf2249eb3caf17c3cbd0cd5bbf4b6ac74d64795
JohanCode2180100 <Johan_CODE21@orange.fr>
2024-02-16 11:26:32 +0100

    add error template in cards

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -6,14 +6,20 @@ import {
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, throwError } from "rxjs";
+import { ErrorComponent } from "./error/error.component";
+import { MatDialog } from "@angular/material/dialog";
 
 @Injectable()
-export class ErrorInterceptor implements ErrorInterceptor {
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private dialog: MatDialog) {}
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log(error);
-        alert(error.error.message);
+        let errorMessage = "Une erreur a été rencontrée";
+        if (error.error.message) {
+          errorMessage = error.error.message;
+        }
+        this.dialog.open(ErrorComponent, { data: { message: errorMessage } });
         return throwError(error);
       })
     );
